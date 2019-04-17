6d58b43b1f5537c35ac5af8a85c040455a805464
Marco Nett <mail@marco-nett.de>
2019-04-17 11:46:33 +0200

    workaround: give bogus path so that index.js is still in-line with upstream but the script still works

--- src/config/foldersAndFiles.js
+++ src/config/foldersAndFiles.js
@@ -5,5 +5,5 @@ export function foldersAndFiles(currentAppName, newName) {
   const nS_CurrentAppName = currentAppName.replace(/\s/g, '');
   const nS_NewName = newName.replace(/\s/g, '');
 
-  return [];
+  return ['nullpath'];
 }
